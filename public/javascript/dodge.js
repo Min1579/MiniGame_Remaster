864793bf080395580b4d65b798e7bc9aec899229
jungyoon huh <gjwjddbsg@gmail.com>
2019-11-28 14:37:43 +0900

    minor patch

--- public/javascript/dodge.js
+++ public/javascript/dodge.js
@@ -1,108 +1,100 @@
 // window.onload = function(){
 //   document.querySelector("#win-cvs")
 // }
+const cnt3 = new Image();
+cnt3.src = "../images/dodge/3-128.png";
+const cnt2 = new Image();
+cnt2.src = "../images/dodge/2-128.png";
+const cnt1 = new Image();
+cnt1.src = "../images/dodge/1-128.png";
+var imgBall = new Image();
+imgBall.src="../images/dodge/ball.png"
+var imgPlane = new Image();
+const overImg = new Image();
+overImg.src = "../images/dodge/over.png";
 
 function init(){
-  var ctx = document.querySelector('#canvas').getContext('2d');  
+  imgPlane.src = "../images/dodge/plane.png"
+  document.querySelector('#msg').innerHTML = 'Enjoy!'
+  const ctx = document.querySelector('#canvas').getContext('2d');  
+  ctx.clearRect(0,0,600,400);
   document.querySelector('button').outerHTML = '';
   for (var i =0; i < 500; i ++){
     clearInterval(i);
   }
-  var cnt3 = new Image();
-  cnt3.src = "../images/dodge/3-128.png";
-  var cnt2 = new Image();
-  cnt2.src = "../images/dodge/2-128.png";
-  var cnt1 = new Image();
-  cnt1.src = "../images/dodge/1-128.png";
-  
   setTimeout(function(){
-    ctx.fillRect(0,0,600,400);
     ctx.drawImage(cnt3,225,125,150,150);
     setTimeout(function(){
       ctx.clearRect(0,0,600,400);
-      ctx.fillRect(0,0,600,400);
       ctx.drawImage(cnt2,225,125,150,150);
       setTimeout(function(){
-        ctx.fillRect(0,0,600,400);
+        ctx.clearRect(0,0,600,400);
         ctx.drawImage(cnt1,225,125,150,150);
         setTimeout(function(){
-          ctx.fillRect(0,0,600,400);
+          ctx.clearRect(0,0,600,400);
           var game = new Game();
         },750)
       },750)
     },750)
   },100)
-
-  
 }
 
 function Game(){
   var canvas = document.querySelector('#canvas')
-  var ballcnt = 1;
   var score = 0;
-  var plane = new Plane(canvas);
-  drawPlane(canvas,plane);
+  var plane = new Plane();
+  var ballArr = [new Ball()];
+  setInterval(function(){
+    drawPlane(canvas,plane,imgPlane);
+    for(var i = 0; i<ballArr.length; i++){
+      drawBall(canvas,plane,ballArr[i]);
+    }
+  },1);
+  setInterval(function () {
+    ballArr.push(new Ball());
+  },1000);
+
   setInterval(function () {
     var ctx = canvas.getContext('2d');
-    ctx.clearRect(0,0,600,400);
     score +=10;
     output = score;
     document.getElementById('score').innerHTML = output;
     var level = ((ballArr.length/10)+1);
     document.getElementById('ball').innerHTML = parseInt(level);
-    ctx.fillRect(0,0,600,400);
+    ctx.clearRect(0,0,600,400);
   },50);
 
-  var ballArr = [new drawBall(canvas,plane)];
-  setInterval(function () {
-    ballArr.push(new drawBall(canvas,plane));
-  },1000);
+
+}
+function Ball(){
+  this.x = Math.random()*250;;
+  this.y = Math.random()*150;;
+  this.xcnt = Math.random()*1+0.5;
+  this.ycnt = Math.random()*1+0.5;
 }
 
-
-function drawBall(canvas,plane){
-  var imgBall = new Image();
-  imgBall.src="../images/dodge/ball.png"
-  var x = Math.random()*250;
-  var y = Math.random()*150;
+function drawBall(canvas,plane,ball){
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-    var xcnt = Math.random()*1+0.5;
-    var ycnt = Math.random()*1+0.5;
-    this.move = setInterval(function(){
-      x += xcnt;
-      y += ycnt;
-      if (x < 1 || x>590){
-        xcnt = -xcnt
+    ctx.drawImage(imgBall, ball.x-5, ball.y-5, 5, 5);
+      ball.x += ball.xcnt;
+      ball.y += ball.ycnt;
+      if (ball.x < 1 || ball.x>590){
+        ball.xcnt = -ball.xcnt
       }
-      if (y < 1 || y>390){
-        ycnt = -ycnt
+      if (ball.y < 1 || ball.y>390){
+        ball.ycnt = -ball.ycnt
       }
-      if (Math.abs(plane.x-x)<10 && Math.abs(plane.y-y)<10){
-          const xPos = plane.x;
-          const yPos = plane.y;
-          plane.imgPlane.src = "../images/dodge/boom.gif";
-          window.clearInterval(1);
-          window.clearInterval(2);
-          setInterval(function(){
-            ctx.clearRect(0,0,600,400);
-            ctx.fillRect(0,0,600,400);
-            ctx.drawImage(plane.imgPlane, xPos-20, yPos-20,40, 40);
-          },1);
+      if (Math.abs(plane.x-ball.x)<10 && Math.abs(plane.y-ball.y)<10){
+          imgPlane.src = "../images/dodge/boom.gif";
           setTimeout(function(){
             for (var i =0; i < 500; i ++){
               clearInterval(i);
             }
-            const overImg = new Image();
-            overImg.onload = function(){
-              ctx.drawImage(overImg,150,100,300,200);
-            }
-            overImg.src = "../images/dodge/over.png";
+            ctx.drawImage(overImg,150,100,300,200);
             var btn = document.createElement("button");
-            document.body.appendChild(btn);
-            document.querySelector('button').outerHTML = '<button style="top:65%;" onclick="init()">Restart?</button>';
-
-            
+            document.querySelector('.btn-wrapper').appendChild(btn);
+            document.querySelector('button').outerHTML = '<button onclick="init()" class="game">Restart?</button>';
             const email = document.querySelector('#user').innerHTML;
             
             if (email ==="Guest"){
@@ -112,8 +104,6 @@ function drawBall(canvas,plane){
               const score = parseInt(document.querySelector('#score').innerText);
               sendAjax("http://localhost:3000/ajax/score",email,score);
             }
-
-
             function sendAjax(url,email,score){
               var data = JSON.stringify({'email':email , 'score':score});
               var xhr = new XMLHttpRequest();
@@ -125,28 +115,29 @@ function drawBall(canvas,plane){
                 var getData = JSON.parse(xhr.responseText);
                 document.querySelector('#msg').innerHTML = getData.msg;
               })
-
             }
-            
-
           },500);
       }
-      ctx.drawImage(imgBall, x-5, y-5, 5, 5);
-    },1)
   }
 }
+
 function Plane(){
-  this.imgPlane = new Image();
-  this.imgPlane.src = "../images/dodge/plane.png"
   this.x = 300;
   this.y = 200;
   this.xcnt = 0;
   this.ycnt = 0;
 }
 
-function drawPlane(canvas,plane){
+function drawPlane(canvas,plane,img){
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, plane.x-15, plane.y-15, 30, 30);
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
+
+  if (canvas.getContext){
+      plane.x < 1 ? plane.x=2 : (plane.x > 580 ? plane.x = 579 : plane.x = plane.x + plane.xcnt);
+      plane.y < 1 ? plane.y=2 : (plane.y > 380 ? plane.y = 379 : plane.y = plane.y + plane.ycnt);
+  }
   function keyDownHandler(e) {
     if(e.keyCode == 39) {
       plane.xcnt = 1;
@@ -161,7 +152,6 @@ function drawPlane(canvas,plane){
       plane.ycnt = -1.5;
     }
   }
-
   function keyUpHandler(e) {
     if(e.keyCode == 39) {
       plane.xcnt = 0;
@@ -176,12 +166,4 @@ function drawPlane(canvas,plane){
       plane.ycnt = 0;
     }
   }
-  if (canvas.getContext){
-  var ctx = canvas.getContext('2d');
-  setInterval(function () {
-    plane.x < 1 ? plane.x=2 : (plane.x > 580 ? plane.x = 579 : plane.x = plane.x + plane.xcnt);
-    plane.y < 1 ? plane.y=2 : (plane.y > 380 ? plane.y = 379 : plane.y = plane.y + plane.ycnt);
-    ctx.drawImage(plane.imgPlane, plane.x-15, plane.y-15,30, 30);
-  },1)
-}
 }
