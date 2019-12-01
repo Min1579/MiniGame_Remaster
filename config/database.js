7adfbafe0a795d2dac2adcdaf964236d1483ef06
min <hsm012362@gmail.com>
2019-12-02 06:34:32 +0900

    database deploy

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
     port: 3306,
-    user: 'root',
-    password: 'Seungmin20*',
-    database: 'minigame'
+    user: 'b0115c6bec9292',
+    password: 'fb148cef',
+    database: 'heroku_121397ed296abf5'
 });
 
 connection.connect();
