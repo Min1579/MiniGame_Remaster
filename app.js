2c94bf3d01f780c0d709f0355cff31161d52b2c1
min <hsm012362@gmail.com>
2019-12-02 03:15:46 +0900

    deploy test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 
 const io = require('socket.io')(server);
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT,'0.0.0.0');
 app.set('port', port);
 
 server.listen(port);
