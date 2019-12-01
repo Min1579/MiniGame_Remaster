46d42fd7d1ad4d5153ae55a16de88994da33950f
min <hsm012362@gmail.com>
2019-12-01 20:01:47 +0900

    remove bcrypt

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const passport = require('passport');
 const LocalStrategy= require('passport-local').Strategy;
 const connection = require('../config/database');
