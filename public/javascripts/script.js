dab3b6056fa858f3de19769a9178ea232b78b32a
min <hsm012362@gmail.com>
2019-12-02 15:30:55 +0900

    set socket start

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-const socket = io()
+const socket = io('https://minigameheaven.herokuapp.com')
 const messageContainer = document.getElementById('message-container')
 const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
