fe8464a60c2e77953965bbbd0cf6223482d31b01
moonhyun94 <greenzip2004@gmail.com>
2019-12-05 16:29:57 +0900

    restart complete

--- public/js/desertwar.js
+++ public/js/desertwar.js
@@ -349,12 +349,23 @@ enemyImg2.src="../images/desertwar/enemy2.png";
 var enemyImgs=[enemyImg2,enemyImg1];
 var enemyList = [];
 var enemycount = 0;
+var count = 0;
+var enemyDie = false;
+
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+var attackPressed = false;
+
+var missileImg = new Image();
+missileImg.src="../images/desertwar/missile.png";
+var missileList=[];
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var enemySpeed = Math.floor((Math.random()*20)+1);
 var initspeed = 5
-
 var stopMove = 0;
 var running = false;
 var gameover =0;
@@ -365,17 +376,18 @@ const MODE_OVER = 2;
 var score = 0;
 
 var back1X=0;
-var back2X =-1500;
-var unitX = 100;
+var back2X =-1000;
+var unitX = 50;
 var unitY = 350;
 
 function desert_init(){
-    ctx.drawImage(backImg,0,0,1500,700);
-    ctx.drawImage(backImg,-1500,0,1500,700);
+    ctx.drawImage(backImg,0,0,1000,700);
+    ctx.drawImage(backImg,-1000,0,1000,700);
     ctx.fillText("SCORE : "+score,5,20);
+    
 }
 function pilot_init(){
-    ctx.drawImage(pilotImg2,100,350,100,100);
+    ctx.drawImage(pilotImg2,unitX,unitY,100,100);
 }
 function enemy_init(){
     enemyList = [];
@@ -399,7 +411,7 @@ function enemy_init(){
         }
         for(var i=0; i<5; i++){
             obj={};
-            obj.x=1500;    
+            obj.x=1000;    
             obj.y=enemyY[i];
             var result = Math.floor(Math.random()*2);
             obj.type = result;
@@ -420,13 +432,14 @@ function enemy_speed(){
         tmp2.x = tmp2.x - initspeed;
         }
     }
-
+var mvp;
 function init(){
     //if(running == false){
     desert_init();
     pilot_init();
     enemy_init();
     enemy_speed();
+    mvp =  setInterval(movePlane,10);
     score=0;
     running = true;
     document.getElementById('play_btn').outerHTML = '<button id="play_btn" onclick="Game()" >Play!</button>';
@@ -443,18 +456,20 @@ function restart(){
 function Game(){
     document.querySelector("#play_btn").outerHTML = ''
     if(Mode == MODE_GAME){
+        
         var ID2 = setInterval(()=>{
-            ctx.drawImage(backImg,back1X,0,1500,700);
-            ctx.drawImage(backImg,back2X,0,1500,700);
+            count++;
+            ctx.drawImage(backImg,back1X,0,1000,700);
+            ctx.drawImage(backImg,back2X,0,1000,700);
             ctx.fillText("SCORE : "+score,5,20);
             back1X -= 5;
             back2X -= 5;
-            if(back1X <= -1500){
+            if(back1X <= -1000){
                 back1X = 0;
-                back2X = -1500;
+                back2X = -1000;
             }
-            if(back2X <= -1500){
-                back2X = 1500;
+            if(back2X <= -1000){
+                back2X = 1000;
                 back1X =0;
               }
             ctx.drawImage(pilotImg2,unitX-50,unitY-50,100,100);
@@ -479,7 +494,7 @@ function Game(){
             }
             for(var i=0; i<5; i++){
                 obj={};
-                obj.x=1500;    
+                obj.x=1000;    
                 obj.y=enemyY[i];
                 var result = Math.floor(Math.random()*2);
                 obj.type = result;
@@ -497,24 +512,38 @@ function Game(){
                         var tmp = enemyList[i];
                         if(tmp.isDead){
                             enemyList.splice(i,1);
-                            enemyDie = ture;
+                            enemyDie = true;
+                           
                         }
                     }
+            //미사일 생성
+            for(var i=0;i<missileList.length;i++){
+                var tmp = missileList[i];
+                ctx.drawImage(missileImg,tmp.x-40,tmp.y-40,50,50)
+                }
+                 moveMissile();
+                 checkMissile();
+                 fire();
+                 checkMissHit();                       
                     for(var i=0; i<enemyList.length;i++){
                         var tmp = enemyList[i];
                         var isPilotDie = unitX > tmp.x-50&&unitX<tmp.x+50&&unitY>tmp.y-50&&unitY<tmp.y+50;
                         if(isPilotDie){
                             restart();
-                            clearInterval(ID2)
+                            clearInterval(ID2);
+                            clearInterval(mvp);
+                            ctx.drawImage(explode,unitX-50,unitY-50,100,100);
                             stopMove = 1;
                             gameover = 1;
+                            back1X = 0;
+                            back2X = -1000;
+                            unitX = 50;
+                            unitY = 350;
                             pilotDie = true;
-                            ctx.drawImage(explode,unitX-50,unitY-50,100,100);
                             sendScore("http://localhost:3000/desertwar/sendScore",score);
-                            
                             }
-                    }
-        },20)
+                        }
+                    },20)
 
        var enemv = setInterval(()=>{
         if(gameover == 1){
@@ -536,50 +565,112 @@ function Game(){
     },20)
     }
 }
-var rightPressed = false;
-var leftPressed = false;
-var upPressed = false;
-var downPressed = false;
-var attackPressed = false;
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-    
-        function keyDownHandler(e) {
-            if(e.keyCode == 39) {
-                rightPressed = true;
+function makeMissile(){
+    if(count%3 != 0){
+                return;
             }
-            if(e.keyCode == 37) {
-                leftPressed = true;
-            }
-            if(e.keyCode == 38){
-             upPressed = true;   
-            }
-            if(e.keyCode == 40){
-                downPressed = true;
-            }
-            if(e.keyCode == 65){
-                attackPressed = true;
-            }
-            }
-            function keyUpHandler(e) {
-            if(e.keyCode == 39) {
-                rightPressed = false;
-            }
-            if(e.keyCode == 37) {
-                leftPressed = false;
-            }
-            if(e.keyCode == 38){
-                upPressed = false;
-            }
-            if(e.keyCode == 40){
-                downPressed = false;
-            }
-            if(e.keyCode == 65) {
-                attackPressed = false;
+            var obj = {};
+            obj.x = unitX;
+            obj.y = unitY+15;
+            obj.isDead = false;
+            missileList.push(obj);
+            for(var i=0; i<missileList.length;i++){
+                        var tmp=missileList[i];
+                        tmp.x=tmp.x+100;
+                        if(tmp.x >= 1000){
+                            tmp.isDead=true;
+                        }
+                    }
+                }
+                function moveMissile(){    
+                for(var i=0; i<missileList.length;i++){
+                            var tmp=missileList[i];
+                            tmp.x=tmp.x+100;
+                            if(tmp.x >= 1000){
+                                tmp.isDead=true;
+                            }
+                        }
+                    }
+                    function checkMissile(){
+    for(var i=missileList.length-1;i>=0;i--){
+        var tmp = missileList[i];
+        if(tmp.isDead){
+            missileList.splice(i,1);
+        }
+    }
+}
+function checkMissHit(){
+        for(var i=0; i<missileList.length;i++){
+            var miss = missileList[i];
+            for(var j=0; j<enemyList.length;j++){
+                var enemy = enemyList[j];
+                var isShooted = miss.x>enemy.x-50&&miss.x<enemy.x+50&&miss.y>enemy.y-50&&miss.y<enemy.y+50;
+                if(isShooted){
+                    miss.isDead=true;
+                    enemy.energy -= 50;
+                    if(enemy.energy <= 0){
+                        enemy.isDead = true;
+                        if(enemy.isDead){
+                            ctx.drawImage(explode,enemy.x-50,enemy.y-50,100,100)
+                        }
+                        if(enemy.type ==0 ){
+                            score += 10;
+                        } else if(enemy.type == 1){
+                            score+=20;
+                        }
+                    }
+                }
             }
         }
+    }
+    
+
+
+
+function fire(){
+    if (attackPressed) {
+    makeMissile();
+}
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+    if(e.keyCode == 38){
+     upPressed = true;   
+    }
+    if(e.keyCode == 40){
+        downPressed = true;
+    }
+    if(e.keyCode == 65){
+        attackPressed = true;
+    }
+    }
+    function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+    if(e.keyCode == 38){
+        upPressed = false;
+    }
+    if(e.keyCode == 40){
+        downPressed = false;
+    }
+    if(e.keyCode == 65) {
+        attackPressed = false;
+    }
+}
+        
     function movePlane(){
+ 
     if(rightPressed) {
             unitX += 7;} 
         if(leftPressed) {
@@ -597,6 +688,5 @@ document.addEventListener("keyup", keyUpHandler, false);
             if(unitY >= 650){
                 downPressed = false;
                 unitY = 650;}}
-        if(stopMove == 1){
-            clearInterval(movePlane);}
-    } var mv =setInterval(movePlane,10) 
+            }
+            
