a7f99a3111a67461b5dfb7e07c20ba0981f668c4
moonhyun94 <greenzip2004@gmail.com>
2019-12-16 15:19:28 +0900

    sound

--- public/js/desertwar.js
+++ public/js/desertwar.js
@@ -10,11 +10,17 @@ enemyImg1.src = "../images/desertwar/enemy.png";
 enemyImg2.src = "../images/desertwar/enemy2.png";
 var gamefinish = new Image();
 gamefinish.src = "../images/desertwar/gameover.png";
+
+var bgm = new Audio("../images/desertwar/desertbgm.mp3");
+var missileSound = new Audio("../images/desertwar/missile.mp3");
+var gameoversound = new Audio("../images/desertwar/gameover.mp3");
+var enemyDieSound = new Audio("../images/desertwar/enemydie.mp3");
+var pilotDieSound = new Audio("../images/desertwar/pilotdie.mp3");
+
 var enemyImgs = [enemyImg2, enemyImg1];
 var enemyList = [];
 var enemycount = 0;
 var count = 0;
-var enemyDie = false;
 
 var rightPressed = false;
 var leftPressed = false;
@@ -25,35 +31,34 @@ var attackPressed = false;
 var missileImg = new Image();
 missileImg.src = "../images/desertwar/missile.png";
 var missileList = [];
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var enemySpeed = 5;
-var initspeed = 5
-var stopMove = 0;
-var running = false;
-var gameover = 0;
+//var gameover = 0;
 
-var Mode = 1;
-const MODE_GAME = 1;
-const MODE_OVER = 2;
-var pilotDie = false;
-var user;
 var score = 0;
 var level = 1;
 var lvlsec = 0;
+var enemySpeed = 5;
 
 var back1X = 0;
 var back2X = -1000;
 var unitX = 50;
 var unitY = 350;
 
+var mvp;
+var levelup;
+// init functions
 function desert_init() {
-    ctx.drawImage(backImg, 0, 0, 1000, 700);
-    ctx.drawImage(backImg, -1000, 0, 1000, 700);
-    ctx.fillText("SCORE : " + score, 5, 20);
+    back1X = 0;
+    back2X = -1000;
+    ctx.drawImage(backImg, back1X, 0, 1000, 700);
+    ctx.drawImage(backImg, back2X, 0, 1000, 700);
 }
 function pilot_init() {
+    unitX = 50;
+    unitY = 350;
     ctx.drawImage(pilotImg2, unitX, unitY, 100, 100);
 }
 function enemy_init() {
@@ -104,11 +109,11 @@ function enemy_speed() {
         tmp2.x = tmp2.x - 5;
     }
 }
-var mvp;
-var levelup;
+
 function init() {
     const name = document.querySelector('#user').innerHTML;
     getScore(`http://${window.location.hostname}:${window.location.port}/desertwar/getScore`,name);
+    bgm.play();
     desert_init();
     pilot_init();
     enemy_init();
@@ -117,7 +122,6 @@ function init() {
     levelup = setInterval(LVLUP,500);
     mvp = setInterval(movePlane, 10);
     score = 0;
-    running = true;
     document.getElementById('play_btn').outerHTML = '<button id="play_btn" onclick="Game()" >Play!</button>';
     Game();
 }
@@ -125,20 +129,21 @@ function init() {
 function restart() {
     var btn = document.createElement('button');
     document.querySelector('#overlay').appendChild(btn);
-    document.querySelector('button').outerHTML = '<button class="btn btn-default" id="play_btn" onclick="init()" >Restart!</button>';
+    document.querySelector('button').outerHTML = '<button class="btn btn-default" id="play_btn" onclick="init()">Restart!</button>';
+    bgm.currentTime = 0;
 }
+// Game() start
 function Game() {
     document.querySelector("#play_btn").outerHTML = ''
-    if (Mode == MODE_GAME) {
         var ID2 = setInterval(() => {
             count++;
             ctx.drawImage(backImg, back1X, 0, 1000, 700);
             ctx.drawImage(backImg, back2X, 0, 1000, 700);
-            
-            ctx.font = ("20px Arial bold");
-            ctx.fillStyle = "blue";
+            ctx.drawImage(pilotImg2, unitX - 50, unitY - 50, 100, 100);
+
             document.getElementById('score').innerHTML = score;
             document.getElementById('level').innerHTML = level;
+            // 배경 스크롤
             back1X -= 5;
             back2X -= 5;
             if (back1X <= -1000) {
@@ -149,15 +154,13 @@ function Game() {
                 back2X = 1000;
                 back1X = 0;
             }
-            ctx.drawImage(pilotImg2, unitX - 50, unitY - 50, 100, 100);
+            // 적기 생성
             enemycount++;
             for (var i = 0; i < enemyList.length; i++) {
                 var tmp = enemyList[i];
                 ctx.drawImage(enemyImgs[tmp.type], tmp.x - 50, tmp.y - 50, 100, 100);
             }
-
             var ranNum = Math.random() * 50;
-
             var result = Math.floor(ranNum);
             if (enemycount == 100) {
                 var enemyY = [600,500,400, 300,200,100,50];
@@ -186,11 +189,11 @@ function Game() {
                     enemycount = 0;
                 }
             }
+            //적기 제거
             for (var i = enemyList.length - 1; i >= 0; i--) {
                 var tmp = enemyList[i];
                 if (tmp.isDead) {
                     enemyList.splice(i, 1);
-                    enemyDie = true;
                 }
             }
             //미사일 생성
@@ -198,10 +201,19 @@ function Game() {
                 var tmp = missileList[i];
                 ctx.drawImage(missileImg, tmp.x - 40, tmp.y - 40, 50, 50)
             }
+            // 적기 속도 제어
+            for (var i = 0; i < enemyList.length; i++) {
+                var tmp = enemyList[i];
+                tmp.x = tmp.x - enemySpeed;
+                if (tmp.x <= -50) {
+                    tmp.isDead = true;
+                }
+            }
             moveMissile();
             checkMissile();
             fire();
             checkMissHit();
+            // 파일럿 충돌
             for (var i = 0; i < enemyList.length; i++) {
                 var tmp = enemyList[i];
                 var isPilotDie = unitX > tmp.x - 50 && unitX < tmp.x + 50 && unitY > tmp.y - 50 && unitY < tmp.y + 50;
@@ -212,42 +224,35 @@ function Game() {
                     clearInterval(levelup);
                     ctx.drawImage(explode, unitX - 50, unitY - 50, 100, 100);
                     ctx.drawImage(gamefinish,250,100,500,300);
-                    stopMove = 1;
-                    gameover = 1;
-                    back1X = 0;
-                    back2X = -1000;
-                    unitX = 50;
-                    unitY = 350;
-                    pilotDie = true;
+                    //gameover = 1;
+                    bgm.pause();
+                    pilotDieSound.play();
+                    gameoversound.play();
+                    //데이터베이스
                     const name = document.querySelector('#user').innerHTML;
                     const highScore = parseInt(document.querySelector('#highScore').innerText);
                     const email = document.querySelector("#email").innerText;
-
-                    if (highScore == 0){
+                    if (name === "Guest") {}
+                    else {
+                      if (highScore == 0){
                         insertScore(`http://${window.location.hostname}:${window.location.port}/desertwar/insertScore`,email, name, score);
-                    } else if (highScore < score) {
+                      }
+                      else if (highScore < score){
                         updateScore(`http://${window.location.hostname}:${window.location.port}/desertwar/updateScore`,email, score);
+                      }
                     }
                 }
             }
         }, 20)
-        var enemv = setInterval(() => {
-            if (gameover == 1) {
-                clearInterval(enemv);
-                gameover = 0;
-            }
-            else {
-                for (var i = 0; i < enemyList.length; i++) {
-                    var tmp = enemyList[i];
-                    tmp.x = tmp.x - enemySpeed;
-                    if (tmp.x <= -50) {
-                        tmp.isDead = true;
-                    }
-                }
-            }
-        }, 20)
-    }
-}
+        //적기속도
+        // var enemv = setInterval(() => {
+        //     if (gameover == 1) {
+        //         clearInterval(enemv);
+        //         gameover = 0;
+        //     }
+            //else {}
+       // }, 20)
+} //Game(); end
 
 function makeMissile() {
     if (count % 8 != 0) {
@@ -296,6 +301,7 @@ function checkMissHit() {
                     enemy.isDead = true;
                     if (enemy.isDead) {
                         ctx.drawImage(explode, enemy.x - 50, enemy.y - 50, 100, 100)
+                        enemyDieSound.play();
                     }
                     if (enemy.type == 0) {
                         score += 10;
@@ -313,18 +319,20 @@ function LVLUP(){
         level++;
         lvlsec = 0;
     }
-    if (level >= 5) {
-        enemySpeed = Math.random() * 20 + 5;    
-    } else {
+    if (level < 5) {
     enemySpeed += 0.2;
+    } else if (level >=5 && level <10) {
+        enemySpeed = Math.random() * 20 + 5;    
+    } else if (level >= 10) {
+        enemySpeed = Math.random() * 20 + 20; 
     }
 }
 function fire() {
     if (attackPressed) {
         makeMissile();
+        missileSound.play();
     }
 }
-       
 
 function getScore(url,name){
     var data = JSON.stringify({'name':name});
@@ -339,6 +347,7 @@ function getScore(url,name){
       document.querySelector('#highScore').innerHTML = getData.score;
     })
   }
+  
   function insertScore(url, email, name, score) {
     var data = JSON.stringify({'email':email, 'name': name, 'score': score});
     var xhr = new XMLHttpRequest(); 
@@ -351,6 +360,7 @@ function getScore(url,name){
       document.querySelector('#highScore').innerHTML = score;
     })
   }
+  
   function updateScore(url, email, score) {
     var data = JSON.stringify({'email':email, 'score': score});
     var xhr = new XMLHttpRequest(); 
