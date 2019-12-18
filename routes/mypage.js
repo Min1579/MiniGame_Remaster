11ea0b006bb6d726368c8343fc709ae8a5718733
min <hsm012362@gmail.com>
2019-12-18 19:16:47 +0900

    complete board

--- routes/mypage.js
+++ routes/mypage.js
@@ -8,22 +8,26 @@ router.get('/', (req,res) => {
     if(!req.user) {
         res.redirect('/login');
     } else {
-        pool.getConnection
-
-        const query = connection.query('select email from user where name = ?', [req.user], (err, rows) => {
-            if(err) throw err;
-            res.render('mypage/main',{'email': rows[0].email, 'name':req.user});
-        })   
+        pool.getConnection((err,conn) => {
+            conn.query('select email from user where name=?', [req.user],(err,rows) => {
+                const email = rows[0].email;
+                conn.release();
+                res.render('mypage/main', {email:email, name:req.user});
+            })
+        })
     }
 })
 
 router.post('/update', (req,res) => {
-    const comment = req.body.commnet;
+    const comment = req.body.comment;
     const email = req.body.email;
-    pool.getConnection(err,conn => {
+    console.log('####',comment);
+    console.log('####',email);
+    
+    pool.getConnection((err,conn) => {
         conn.query('update user set comment = ? where email = ? ',[comment, email], (err,rows)=> {
             conn.release();
-
+            res.redirect('/mypage');
         })
     })
 })
