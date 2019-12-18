beac69f475f7d21f91ac7c44d40d585aeb92070b
min <hsm012362@gmail.com>
2019-12-19 00:47:23 +0900

    check

--- config/database.js
+++ config/database.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'us-cdbr-iron-east-05.cleardb.net',
+    host: 'localhost',
     port: 3306,
-    user: 'b0115c6bec9292',
-    password: 'fb148cef',
-    database: 'heroku_121397ed296abf5'
+    user: 'root',
+    password: 'Seungmin20*',
+    database: 'minigame'
 });
-
 
 module.exports = pool;
