80921f8c8c1c7c01be8116c932cfb1f32607c43b
min <hsm012362@gmail.com>
2019-12-19 01:25:06 +0900

    datebase set

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
     port: 3306,
-    user: 'root',
-    password: 'Seungmin20*',
-    database: 'minigame'
+    user: 'b591c460fb7aa2',
+    password: '1714412a',
+    database: 'heroku_0d58aebdfff05c1'
 });
 
 module.exports = pool;
